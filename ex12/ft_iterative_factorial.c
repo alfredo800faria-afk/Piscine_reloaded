c1d0e7ab8703d13c3f6a9bf400728a25f966da77
alfredo800faria <alfredo800faria@gmail.com>
2025-10-15 20:11:46 +0100

    ft_iterative_factorial.c

---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,19 @@
+int ft_iterative_factorial(int nb)
+{
+    int result;
+    int i;
+
+    if (nb < 0)
+        return 0;
+    if (nb == 0 || nb == 1)
+        return 1;
+    
+    result = 1;
+    i = 2;
+    while (i <= nb)
+    {
+        result *= i;
+        i++;
+    }
+    return result;
+}
